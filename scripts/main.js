224d077cb656ecf91c1545c54cb27145baa05ea0
gaku <gaku.i.913@gmail.com>
2021-06-03 12:10:53 +0900

    モバイルメニューのリンクをクリックしたらメニューを閉じる

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,17 @@
 const header = document.querySelector('header');
 const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+const menuLinks = document.querySelectorAll('.menu a');
 
-mobileMenuIcon.addEventListener('click', function() {
+const menuOpenToggle = function() {
   header.classList.toggle('menu-open');
+}
+
+const menuClose = function() {
+  header.classList.remove('menu-open');
+}
+
+mobileMenuIcon.addEventListener('click', menuOpenToggle);
+
+menuLinks.forEach(menuLink => {
+  menuLink.addEventListener('click', menuClose);
 });
